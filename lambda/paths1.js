f038173fc057775e333f24b4d3769b85b021d339
make14 <62626890+make14@users.noreply.github.com>
2021-05-19 03:55:55 -0500

    .

--- lambda/paths1.js
+++ lambda/paths1.js
@@ -1,16 +1,14 @@
+// sample input:       M5,15 c5.5,0 10-4.5 10,-10 h10
 exports.handler = async function(event, context) {
-
     const { pathDataToPolys } = require('svg-path-to-polygons');
-    var pathData;
-    //pathData = 'M5,15 c5.5,0 10-4.5 10,-10 h10';  // sample data
-
 
     if (event.httpMethod != "POST") {
       return ({ statusCode: 200, body: "POST method accepted only\n" });
     }
-    pathData = JSON.parse(event.body);   // POST requests
 
-    let points = pathDataToPolys(pathData, {tolerance:1, decimals:1});
+    let points = pathDataToPolys(
+        event.body,
+        {tolerance:1, decimals:1});
 
     return({
         statusCode: 200,

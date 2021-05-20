93b2089e00c77b959345ae0de34650b63d6f6dce
make14 <62626890+make14@users.noreply.github.com>
2021-05-20 03:45:37 -0500

    .

--- lambda/paths1.js
+++ lambda/paths1.js
@@ -1,4 +1,8 @@
-// sample input:       M5,15 c5.5,0 10-4.5 10,-10 h10
+// sample input (a series of paths, each on a different line): 
+//
+// M 432 26 L 607.5 26 L 608 26.5 L 608 68 L 545 68 L 545 263.5 L 544.5 264 L 495 264 L 495 68.5 L 494.5 68 L 432 68 L 432 26 Z 
+// M 641 26 L 805.5 26 L 806 26.5 L 806 68 L 690 68 L 690 122 L 787 122 L 787 164 L 690 164 L 690 222 L 806 222 L 806 264 L 641 264 L 641 26 
+
 exports.handler = async function(event, context) {
     const { pathDataToPolys } = require('svg-path-to-polygons');
 

7555ce71717adef98e62566d9ee6c2f6b532e62f
make14 <62626890+make14@users.noreply.github.com>
2021-05-20 11:35:48 -0500

    .

--- lambda/paths5.js
+++ lambda/paths5.js
@@ -128,10 +128,10 @@ exports.handler = async function(event, context) {
     return({
         statusCode: 200,
         body: ""
-        + "// svg fill paths: " + fillPathsCount + "\n"
-        + "// svg hole paths: " + holePathsCount + "\n"
-        + "// points #: " + pointsCount + "\n"
-        + "// holes  #: " + holesArray.length + "\n"
+        + "// svg fill paths: " + fillPathsCount + " "
+        + "   svg hole paths: " + holePathsCount + "\n"
+        + "// points #: " + pointsCount + "  "
+        + "   holes  #: " + holesArray.length + "\n"
         + "// points: " + JSON.stringify(pointsArray) + "\n"
         + "// holes : " + JSON.stringify(holesArray) + "\n"
         + "// triangles: \n/*\n" +  printTriangles(triangles) + "*/\n"
